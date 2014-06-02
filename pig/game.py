2cba9abe2c42a682e2f53b72b0e7272189c76423
7t <githabber@gmail.com>
2014-06-02 17:16:58 +0400

    test_scores is passed
    methods
        Pig: __init__, get_players, roll, get_scores

--- pig/game.py
+++ pig/game.py
@@ -6,6 +6,9 @@ import random
 class Pig:
     def __init__(self, *players):
         self.players = players
+        self.scores = {}
+        for player in self.players:
+            self.scores[player] = 0
 
     def get_players(self):
         '''Returns a tuple of all players'''
@@ -14,3 +17,6 @@ class Pig:
     def roll(self):
         '''Returns a number between 1 and 6'''
         return random.randint(1, 6)
+
+    def get_scores(self):
+        return self.scores
--- pig/test_game.py
+++ pig/test_game.py
@@ -8,13 +8,19 @@ class GameTest(TestCase):
     def setUp(self):
         self.pig = game.Pig('PlayerA', 'PlayerB', 'PlayerC')
 
-    def test_join(self):
+    def test_get_players(self):
         '''Players may join a game of Pig'''
         self.assertTupleEqual(self.pig.get_players(),
                               ('PlayerA', 'PlayerB', 'PlayerC'))
 
     def test_roll(self):
+        '''A roll of the die results in an integer between 1 and 6'''
         for i in range(500):
             res = self.pig.roll()
             self.assertIsInstance(res, int)
             self.assertTrue(1 <= res <= 6)
+
+    def test_get_scores(self):
+        '''Player scores can be retrieved'''
+        self.assertDictEqual(self.pig.get_scores(),
+                             {'PlayerA': 0, 'PlayerB': 0, 'PlayerC': 0})
